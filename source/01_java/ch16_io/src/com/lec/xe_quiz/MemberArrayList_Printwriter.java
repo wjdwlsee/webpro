ccdfa0c5f1aed179aa75f5c8e4266157a4efc217
wjdwlsee <wjdwlsee@naver.com>
2023-07-20 14:42:53 +0900

    note_java_io

--- source/01_java/ch16_io/src/com/lec/xe_quiz/MemberArrayList_Printwriter.java
+++ source/01_java/ch16_io/src/com/lec/xe_quiz/MemberArrayList_Printwriter.java
@@ -2,8 +2,6 @@ package com.lec.xe_quiz;
 
 import java.util.ArrayList;
 
-public class MemberArrayList_Printwriter {
-	public static void main(String[] args) {
-		ArrayList<Member> members = new Arra 
-	}
+public class MemberArrayList_Printwriter { 
+	
 }
